7442f7326187cb9c231c93fe3a8b494a4d2dd0b5
Alfons Pastor <92100740+prakgon@users.noreply.github.com>
2022-04-29 01:26:41 +0200

    half-working gameloop

--- Assets/Scripts/PlayerScripts/MatchManager.cs
+++ Assets/Scripts/PlayerScripts/MatchManager.cs
@@ -2,23 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using Helpers;
 using Photon.Pun;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class MatchManager : MonoBehaviourPun
 {
+    [SerializeField] private TMP_Text gameOverText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
-    
+
     public int PlayerDied()
     {
         GameOver(Literals.MatchResults.Defeat);
@@ -29,14 +30,26 @@ public class MatchManager : MonoBehaviourPun
     [PunRPC]
     public void GameOver(Literals.MatchResults result)
     {
+        GetComponent<PlayerInput>().enabled = false;
         switch (result)
         {
             case Literals.MatchResults.Victory:
-                Debug.Log("Game Over: Victory");
+                //StartCoroutine(GameOverSequence("VICTORY"));
+                StartCoroutine(nameof(GameOverSequence), "VICTORY");
                 break;
             case Literals.MatchResults.Defeat:
-                Debug.Log("Game Over: Defeat");
+                //StartCoroutine(GameOverSequence("DEFEAT"));
+                StartCoroutine(nameof(GameOverSequence), "DEFEAT");
                 break;
         }
+    }
+
+    private IEnumerator GameOverSequence(string gameOverMessage)
+    {
+        gameOverText.text = gameOverMessage;
+        gameOverText.enabled = true;
+        yield return new WaitForSeconds(10);
+        Debug.Log("disc");
+        PhotonNetwork.Disconnect();
     }
 }
