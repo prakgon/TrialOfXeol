8423baadb0017edc155b8a3da5f590fc0e160392
Alfons Pastor <alfons.pastor.7e4@itb.cat>
2022-04-28 19:17:55 +0200

    wip

--- Assets/Scripts/Helpers/GameManager.cs
+++ Assets/Scripts/Helpers/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using static Helpers.LiteralToStringParse;
 using static Helpers.Literals;
 using UnityEngine;
@@ -10,11 +11,26 @@ namespace Helpers
 {
     public class GameManager : MonoBehaviourPunCallbacks
     {
-
         public GameObject playerPrefab;
         public GameObject freeSpectatorPrefab;
-        #region Photon Callbacks
+        public GameObject[] spawnPoints;
 
+        private static GameManager _instance;
+
+        public static GameManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<GameManager>();
+                }
+
+                return _instance;
+            }
+        }
+
+        #region Photon Callbacks
 
         /// <summary>
         /// Called when the local player left the room. We need to load the launcher scene.
@@ -28,7 +44,9 @@ namespace Helpers
         {
             if (playerPrefab == null)
             {
-                Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+                Debug.LogError(
+                    "<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'",
+                    this);
             }
             else
             {
@@ -36,14 +54,20 @@ namespace Helpers
                 {
                     Debug.LogFormat("We are Instantiating LocalPlayer from {0}", SceneManagerHelper.ActiveSceneName);
                     // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
-                    var userType = (UserTypes) PhotonNetwork.LocalPlayer.CustomProperties[UserType];
+                    var userType = (UserTypes)PhotonNetwork.LocalPlayer.CustomProperties[UserType];
                     switch (userType)
                     {
                         case UserTypes.Player:
-                            PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+                            PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue(ToxSqlProperties.FighterCount,
+                                out object fighterCount);
+                            Debug.Log(fighterCount);
+                            Vector3 spawnPoint = (int) fighterCount > 1 ? spawnPoints[0].transform.position : spawnPoints[1].transform.position;
+                            PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint, Quaternion.identity,
+                                0);
                             break;
                         case UserTypes.FreeSpectator:
-                            PhotonNetwork.Instantiate(freeSpectatorPrefab.name, new Vector3(0f, 5f, -10f), Quaternion.identity, 0);
+                            PhotonNetwork.Instantiate(freeSpectatorPrefab.name, new Vector3(0f, 5f, -10f),
+                                Quaternion.identity, 0);
                             break;
                     }
                 }
@@ -53,6 +77,14 @@ namespace Helpers
                 }
             }
         }
+
+        private void Update()
+        {
+            PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue(ToxSqlProperties.FighterCount,
+                out object fighterCount);
+            Debug.Log(fighterCount);
+        }
+
         public override void OnPlayerEnteredRoom(Player other)
         {
             Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
@@ -60,7 +92,8 @@ namespace Helpers
 
             if (PhotonNetwork.IsMasterClient)
             {
-                Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+                Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}",
+                    PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
 
 
                 //LoadArena();
@@ -75,7 +108,8 @@ namespace Helpers
 
             if (PhotonNetwork.IsMasterClient)
             {
-                Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+                Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}",
+                    PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
 
 
                 //LoadArena();
@@ -87,17 +121,14 @@ namespace Helpers
 
         #region Public Methods
 
-
         public void LeaveRoom()
         {
             PhotonNetwork.LeaveRoom();
         }
 
-
         #endregion
 
         #region Private Methods
-
 
         void LoadArena()
         {
@@ -105,10 +136,10 @@ namespace Helpers
             {
                 Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
             }
+
             Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
             PhotonNetwork.LoadLevel(SampleScene);
         }
-
 
         #endregion
     }
